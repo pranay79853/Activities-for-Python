99dc53491f9d02c2f5a29f5df3888f01e42a007d
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-04-30 12:09:42 +0530

    initial commit

---
+++ Activity_1.py
@@ -0,0 +1,14 @@
+ #Welcome to Python
+print("Welcome to the world of programming")
+#Print a number
+print(4)
+print(5)
+print()
+print("hello world welcome")
+#New line
+print("In Python programming language, if you try to use a keyword as a name, \n the program will give an error.")
+print("First \nSecond")
+#Passing multiple thing in print
+print("hello world", 5)
+#End argument of print
+print("Hello world  ", end = "$")
