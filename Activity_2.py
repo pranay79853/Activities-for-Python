dd21444d23f5d09512000da81aaea3ced85e358a
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-04-30 12:26:43 +0530

    learn variable

---
+++ Activity_2.py
@@ -0,0 +1,10 @@
+#Creating Different Variables
+x=5
+y = "John"
+print(x, "+" , y)
+codingal = "codingal"
+print(codingal)
+# User Input
+name = input("enter your name: ")
+print(name)
+print("\nHello", name, "\nwelcome to codingal")
