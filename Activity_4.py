53c4a7e5253313adc0a8df3af8018924cfbb800c
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-01 12:02:18 +0530

    data types

---
+++ Activity_4.py
@@ -0,0 +1,19 @@
+# 1) Store an integer value in `a`.
+a = "Pranay"
+
+# 2) Print the datatype of `a` using `type()`.
+print(type(a))
+
+# 3) Store a decimal (float) value in `b`.
+b = 1.9
+
+# 4) Print the datatype of `b` using `type()`.
+print(type(b))
+# 5) Store a text (string) value in `c`.
+c = "I live on Earth"
+# 6) Print the datatype of `c` using `type()`.
+print(type(c))
+# 7) Store a boolean value (True/False) in `d`.
+d = False
+# 8) Print the datatype of `d` using `type()`.
+print(type(d))
