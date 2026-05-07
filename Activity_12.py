33af27a06bfaed596820077254d5621cf0548aa2
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-07 11:57:45 +0530

    Checking the number greater or smaller

---
+++ Activity_12.py
@@ -0,0 +1,12 @@
+# check greater or less than 15
+
+input1 = int(input("enter a number : "))
+
+if(input1 > 15) :
+    print("This number is greater than 15.")
+
+elif(input1 < 15) :
+    print("This number is smaller than 15.")
+
+else :
+    print("This number is equal to 15.")
