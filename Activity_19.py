fe8b38c7e3f36074bbbdbe344520ee4b199bfb3a
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-11 11:37:45 +0530

    Operator precedence

---
+++ Activity_19.py
@@ -0,0 +1,20 @@
+# 1) Store values in `v`, `w`, `x`, `y`, and `z`.
+v = 5
+w = 6
+x = 7
+y = 8
+z = 0
+# 2) Calculate the expression (v + w) * x / y and store the result back in `z`.
+z = (v + w) * x / y
+# 3) Print the value of `z` with a message.
+print(f"The value of z is: {z}")
+# 4) Store a name in `name` and a number in `age`.
+name = "Alex"
+age = 3
+# 5) Check this condition using `or` and `and`:
+if name == "Alex" or (name == "John" and age >= 2):
+    print("Hello, Alex or John!")
+else:
+    print("Goodbye!")
+
+# - Otherwise, print the goodbye message.
