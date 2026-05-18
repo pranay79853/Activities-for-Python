59b4d3135a4795d2afe0cd2f63e41356e8d9ad89
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-18 11:29:39 +0530

    Well wishes

---
+++ Activity_44.py
@@ -0,0 +1,7 @@
+# Write a program to create a function named well_wishes that gives output "Hello" and "How are you?"
+def well_wishes():
+    print("Hello")
+    print("How are you?")
+
+# Call the function
+well_wishes()
