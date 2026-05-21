7ada843ba31eadda6626a985d4086a165171ea50
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-21 11:26:04 +0530

    Value Error

---
+++ Activity_53.py
@@ -0,0 +1,7 @@
+# Write a program to understand how value error exception is working
+try:
+    number = int(input("Enter a number: "))
+    print("Entered number is: ", number)
+
+except ValueError as ex:
+    print("Entered number is ", ex)
