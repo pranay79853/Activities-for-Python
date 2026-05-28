1ec214623de88f435edcba1dae90a816238f999c
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-28 11:55:28 +0530

    Get the country code

---
+++ Activity_66.py
@@ -0,0 +1,11 @@
+country_code = {'India' : '0091',
+'Australia' : '0025',
+'Nepal' : '00977'}
+print(country_code)
+print("Country code for India -")
+print(country_code.get('India'))
+print("Country code for Australia -")
+print(country_code.get('Australia'))
+print("Country code for Nepal -")
+print(country_code.get('Nepal'))
+print(country_code.get('Japan', 'Not Found'))
