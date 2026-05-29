758e7901adc1d19d24c5f66e034af68fa960ad74
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-29 11:35:52 +0530

    Set Interaction

---
+++ Activity_68.py
@@ -0,0 +1,12 @@
+# Write a Python program to find the intersection of two sets.
+Set_1 = {"Green", "Blue"}
+Set_2 = {"Blue", "Yellow"}
+print(Set_1)
+print(Set_2)
+
+Set_3 = (Set_1. intersection(Set_2))
+print(Set_3)
+Set_3 = (Set_1. union(Set_2))
+print(Set_3)
+Set_3 = (Set_1. difference(Set_2))
+print(Set_3)
