414fbddc7a69b758b5d153dde53b1abf2870725c
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-30 12:07:59 +0530

    That's the exit

---
+++ Activity_72.py
@@ -0,0 +1,6 @@
+# Write a program where the value of i begins from 1 and goes to 10 when value becomes 5, force the interpreter to exit
+for i in range(10):
+    if i == 5:
+        print(exit)
+        exit()
+    print(i)
