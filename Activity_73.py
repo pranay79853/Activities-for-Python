89cb881947e8d8280aca73eed566339e01f7d5cf
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-06-01 11:32:15 +0530

    Student Class

---
+++ Activity_73.py
@@ -0,0 +1,16 @@
+# Write a program to create a class with the name Student and perform the following tasks:
+
+class Student:
+    def __init__(self, grade):
+        self.grade = grade
+
+Student1 = Student("A")
+print(f"The grade of the student is: {Student1.grade}")
+
+# Declare a variable grade.
+
+# Print a sentence inside the class.
+
+# Create an object of the class Student.
+
+# See the output.
